7157664aeb81a3d12cb5c488e1be300ea8d8c428
noepn <noe.pena@lycee-berthollet.com>
2025-05-27 11:57:18 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,10 +1,17 @@
- const checkbox = document.getElementById("tache1");
-    const input = document.getElementById("texte1");
+// Fonction qui associe toutes les cases à leur input voisin
+document.addEventListener("DOMContentLoaded", () => {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
+  checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", () => {
+      // On prend l'input juste après la checkbox
+      const input = checkbox.nextElementSibling;
+
       if (checkbox.checked) {
-        input.classList.add("input-barre");
+        input.classList.add("strike");
       } else {
-        input.classList.remove("input-barre");
+        input.classList.remove("strike");
       }
     });
+  });
+});
