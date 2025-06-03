c58b2cc0e7a1ef2d60fdc30887c27d1bc9607a90
dim2008 <dimitri.berthet@lycee-berthollet.com>
2025-06-03 11:53:45 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,23 +1,26 @@
 function ajouterLigne() {
-    // Trouver le dernier <p> dans le body
     const body = document.body;
-    const paragraphs = body.querySelectorAll('p');
+    const paragraphs = body.querySelectorAll('p.tache');
     const lastP = paragraphs[paragraphs.length - 1];
-
-    // Cloner la dernière ligne (avec checkbox et input)
     const newP = lastP.cloneNode(true);
 
-    // Nettoyer la nouvelle ligne : décocher checkbox et vider input
     const checkbox = newP.querySelector('input[type="checkbox"]');
     const textInput = newP.querySelector('input[type="text"]');
+
+    // Réinitialiser la checkbox décochée
     checkbox.checked = false;
+    // Nettoyer le texte
     textInput.value = '';
-    
-    // Optionnel : changer l'id de la checkbox pour éviter doublons
-    // Exemple : tache + (nombre de lignes + 1)
+
+    // Enlever le style barré
+    textInput.style.textDecoration = 'none';
+    textInput.style.color = '#000'; // ou couleur normale
+
+    // Enlever la classe tache-fait si tu l'utilises
+    newP.classList.remove('tache-fait');
+
     checkbox.id = 'tache' + (paragraphs.length + 1);
 
-    // Insérer la nouvelle ligne à la fin
     body.insertBefore(newP, body.querySelector('script'));
 }
 
